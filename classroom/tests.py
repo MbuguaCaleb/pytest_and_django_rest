b429e254103ff56c44988684b7106b47626c41c1
Mbugua Caleb <mbuguacaleb30@gmail.com>
2021-09-18 07:57:03 +0300

    leveraging-mixer

--- classroom/tests.py
+++ classroom/tests.py
@@ -1,18 +1,19 @@
 from django.test import TestCase
 from classroom.models import Student
+from mixer.backend.django import mixer
 # Create your tests here.
 
 
 class TestStudentModel(TestCase):
 
     # Helps me avoid repeated code when writing Tests
-    def setUp(self):
-        # i am defining obj as property of the class
-        self.student1 = Student.objects.create(
-            first_name='Caleb',
-            last_name='Mbugua',
-            admission_number=7552
-        )
+    # def setUp(self):
+    #     # i am defining obj as property of the class
+    #     self.student1 = Student.objects.create(
+    #         first_name='Caleb',
+    #         last_name='Mbugua',
+    #         admission_number=7552
+    #     )
 
     def test_add_a_plus_b(self):
         a = 1
@@ -23,44 +24,36 @@ class TestStudentModel(TestCase):
 
     # test if model can be created
     def test_student_can_be_created(self):
-        self.assertEqual(self.student1.first_name, 'Caleb')
+        student1 = mixer.blend(Student, first_name='Caleb')
+        student_result = Student.objects.last()
+
+        self.assertEqual(student_result.first_name, 'Caleb')
 
     # test the str method
     def test_str_return(self):
-        self.assertEqual(str(self.student1), "Caleb")
+        student1 = mixer.blend(Student, first_name='Caleb')
+        student_result = Student.objects.last()
+        self.assertEqual(str(student_result), "Caleb")
 
     def test_grade_fail(self):
-        Student.objects.create(
-            first_name='Caleb',
-            last_name='Mbugua',
-            admission_number=7556,
-            average_score=10
-        )
+        student1 = mixer.blend(Student, average_score=10)
 
         student_result = Student.objects.last()
 
         self.assertEqual(student_result.get_grade(), "Fail")
 
     def test_grade_pass(self):
-        Student.objects.create(
-            first_name='Caleb',
-            last_name='Mbugua',
-            admission_number=7551,
-            average_score=60
-        )
+        student1 = mixer.blend(Student, average_score=60)
 
         student_result = Student.objects.last()
 
         self.assertEqual(student_result.get_grade(), "Pass")
 
     def test_grade_excellent(self):
-        Student.objects.create(
-            first_name='Caleb',
-            last_name='Mbugua',
-            admission_number=7550,
-            average_score=90
-        )
+        student1 = mixer.blend(Student, average_score=90)
 
         student_result = Student.objects.last()
 
         self.assertEqual(student_result.get_grade(), "Excellent")
+
+
--- readme.md
+++ readme.md
@@ -30,6 +30,13 @@ pip install python-decouple
 
 ```
 
+**Running Tests**
+
+```
+python manage.py test
+
+```
+
 **Set Up**
 
 ```
@@ -39,6 +46,30 @@ are replicated across various test cases..
 
 You can even instanticate a class the same way as you
 would a property an use it across various testcases
+
+Some of the Setup functions:
+
+(a)Setting up new users
+(b)Getting access tokens/logged in users
+(c)Setting up timers
+
+```
+
+**Using Mixer**
+
+```
+It is a very Important package in autgenerating Models.
+
+pip install mixer
+
+It helps me save so much time, instead of Hardcoding Models,
+especially the more complex ones.
+
+Where you want to override default models generated , You
+can do as below,
+
+student1 = mixer.blend(Student, average_score=10)
+
 
 ```
 
